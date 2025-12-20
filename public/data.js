bf05fe610ba644b2dbd1baeac1fc3a632afbf9a7
Saban-94 <hsaban2025@gmail.com>
2025-12-20 03:16:50 +0200

    Add product database with details for multiple products

---
+++ public/data.js
@@ -0,0 +1,44 @@
+const productsDB = [
+    {
+        id: "107",
+        name: "SikaTop® Seal 107",
+        shortDesc: "איטום צמנטי גמיש דו-רכיבי",
+        fullDesc: "מערכת איטום צמנטית גמישה, דו רכיבית, המיועדת לאיטום חדרים רטובים, מרפסות, בריכות שחייה ומאגרי מים. בעלת כושר הידבקות מעולה לבטון וטיח.",
+        image: "https://gilar.co.il/wp-content/uploads/2023/12/SikaTop-Seal-107-25-Kg.png",
+        video: "1UPK40Dg4Ew", // YouTube ID
+        price: 180, // מוסתר מהמסך
+        specs: [
+            { icon: "droplets", label: "איטום", value: "חיובי ושלילי" },
+            { icon: "layers", label: "עובי", value: "2-3 מ\"מ" },
+            { icon: "clock", label: "ייבוש בין שכבות", value: "6 שעות" }
+        ]
+    },
+    {
+        id: "255",
+        name: "SikaCeram® 255 StarFlex",
+        shortDesc: "דבק קרמיקה גמיש במיוחד C2TE S1",
+        fullDesc: "דבק צמנטי בהיר, גמיש מאוד, מועשר בפולימרים. מתאים להדבקת אריחי פורצלן, אבן טבעית וקרמיקה על קירות ורצפות, פנים וחוץ. בעל זמן עבידות ארוך.",
+        image: "https://gilar.co.il/wp-content/uploads/2021/04/SikaCeram-255-StarFlex-LD-IL-white-20kg.jpg",
+        video: "kNJeCVrC1AI",
+        price: 55, // מוסתר מהמסך
+        specs: [
+            { icon: "activity", label: "גמישות", value: "S1 (גבוהה)" },
+            { icon: "maximize", label: "עובי הדבקה", value: "עד 10 מ\"מ" },
+            { icon: "shield", label: "תקן", value: "C2TE" }
+        ]
+    },
+    {
+        id: "560",
+        name: "Sikalastic® 560",
+        shortDesc: "ציפוי איטום פוליאוריתני היברידי",
+        fullDesc: "ציפוי איטום נוזלי על בסיס טכנולוגיית Co-Elastic. עמיד UV, ידידותי לסביבה, מתאים לאיטום גגות חדשים ושיקום גגות ישנים. מגשר על סדקים.",
+        image: "https://gilar.co.il/wp-content/uploads/2020/07/Sikalastic-560-20kg-white.jpg",
+        video: "c1OYGL0WbRg",
+        price: 340, // מוסתר מהמסך
+        specs: [
+            { icon: "sun", label: "עמידות UV", value: "מלאה" },
+            { icon: "paint-bucket", label: "צריכה", value: "1.5 ק\"ג/מ\"ר" },
+            { icon: "wind", label: "יישום", value: "רולר/התזה" }
+        ]
+    }
+];
